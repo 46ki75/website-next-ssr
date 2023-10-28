55a90f14bdc21e3edf647cf12f30b61bacd9403d
shirayuki <shirayuki@46ki75.com>
2023-10-28 21:36:49 +0900

    feat: Set up development environment
    - Introduce Sass
    - Configure testing environment with vitest + react-testing-library
    - Add Prettier
    - Implement git hooks with Husky

---
+++ src/app/page.test.tsx
@@ -0,0 +1,11 @@
+import { expect, test, describe } from 'vitest'
+import { render, screen, within } from '@testing-library/react'
+import Home from './page'
+
+describe('Home component', () => {
+  test('home', () => {
+    render(<Home />)
+    const main = within(screen.getByRole('main'))
+    expect(main.getByRole('heading', { level: 2, name: /Docs/i })).toBeDefined()
+  })
+})
